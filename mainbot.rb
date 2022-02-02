2b4239ff25d5ccf2f1c41cc4db66809059da2175
Abdul Hafizdh Kamal <abdulhafidhk@users.noreply.github.com>
2022-02-03 00:11:00 +0700

    Update mainbot.rb

--- mainbot.rb
+++ mainbot.rb
@@ -370,7 +370,7 @@ node 2 = `10.54.128.132`"
 		Open = #{date}
 		Request = #{firstname}";
 		sheetrow=["#{ticket}","","#{title}","#{desc}","OPEN","#{sev}","#{date}","#{date}","#{firstname}"];
-		@issuelog[@issuelog.num_rows][0](sheetrow);
+		@issuelog[@issuelog.num_rows][0] = sheetrow;
 		@issuelog.save;
 		return result+sheetrow.join(',');
 	end
